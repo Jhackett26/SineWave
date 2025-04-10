63a603d0ebb20aaf46c105b0de0a6e853e996436
Jhackett26 <john_hackett26@milton.edu>
2025-04-10 19:13:17 -0400

    final commit

--- src/Main.java
+++ src/Main.java
@@ -1,89 +1,112 @@
 import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Main extends JPanel {
-    int prevY;
-    int prevX;
+    double prevY = 0;
+    double prevX = -650;
     double unchangedPrevY = 0;
+    double prevSlope = 0;
+    public int runs=0;
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
+        runs+=1;
+        if (runs>=4) {
+            boolean isInvertable = true;
+            boolean isOdd = false;
+            ArrayList<Double> Ys = new ArrayList<Double>();
+            ArrayList<Double> Xs = new ArrayList<Double>();
+            ArrayList<Double> slopes = new ArrayList<Double>();
+            ArrayList<Double> slopeChanges = new ArrayList<Double>();
+            int width = getWidth();
+            int height = getHeight();
 
-        // Set the color for drawing
-
-
-        // Get the dimensions of the panel
-        boolean isInvertable = true;
-        boolean isOdd = false;
-        ArrayList<Double> Ys = new ArrayList<Double>();
-        int width = getWidth();
-        int height = getHeight();
-
-
-        // Scale factors to fit the sine wave in the window
-        int amplitude = height / 4; // Amplitude of sine wave (adjust to fit)
-        int frequency = 1; // Frequency multiplier for the wave
-        // Calculate the x-axis (middle of the panel)
-        int xAxis = height / 2;
-        int left_shift = 0;
-        int added_height  = 400;
-
-        // Draw the sine wave
-        g.setColor(Color.BLACK);
-        for (int x = 0; x < width; x++) {
-//            double y = (int)((amplitude * Math.sin(2*(x * 2 *Math.PI * frequency) / width)+added_height)+left_shift);
-//            double y = f(g(x));
-            double y = x;
-            double colorNum = Math.abs((y-unchangedPrevY)*150);
-            unchangedPrevY = y;
-            if(colorNum>255) {
-                colorNum = 255;
+            // Draws axes
+            g.drawLine(0, 430, 1200, 430);
+            g.drawLine(600, 0, 600, 1200);
+            ArrayList<Double> changeInSlope = new ArrayList<Double>();
+            for (double x = (double) -600; x < (double) 600; x++) {
+                double y = f(g(x));
+//                System.out.println("("+x+", "+y+")");
+                Ys.add(y);
+                Xs.add(x);
             }
-            else if(colorNum<0) {
-                colorNum = 0;
+            slopes = fillSlopes(Xs,Ys);
+            slopeChanges = changeInSlopes(slopes);
+
+
+
+          for(int i  = 0; i < slopeChanges.size(); i++){
+              double colorNum = (((slopeChanges.get(i) - Collections.min(slopeChanges))/Collections.max(slopeChanges))*255);
+              unchangedPrevY = Ys.get(i);
+              if (colorNum > 255) {
+                  colorNum = 255;
+              } else if (colorNum < 0) {
+                  colorNum = 0;
+              }
+              Color lineColor = new Color((int) (colorNum), 0, 0);
+              g.setColor(lineColor);
+              g.drawLine(((int) prevX + 600), ((int) -(Ys.get(i)) + 430), ((int) prevX + 600), (int) (-prevY + 430));
+              prevY = Ys.get(i);
+              prevX = Xs.get(i);
             }
-            Color lineColor = new Color((int)(colorNum), 0, 0);
-            g.setColor(lineColor);
-            y=(int)((y*-1)+height);
-            g.drawLine(prevX,(int)(y),prevX,prevY);
-            prevY = (int)(y);
-            prevX = x;
-            Ys.add(y);
+//          checkInflectionPoint(slopeChanges, Xs, Ys);
         }
-        for(int i = 0; i <= Ys.size()-1;i++) {
-            for(int n = 0; n <= Ys.size()-1; n++) {
-                if (n != i && Ys.get(n).equals(Ys.get(i))) {
-                    isInvertable = false;
-                    break;
+        }
+        public ArrayList<Double> fillSlopes(ArrayList <Double> Xs, ArrayList <Double> Ys){
+            ArrayList<Double> slopes = new ArrayList<Double>();
+            for (int i = 0; i < Xs.size(); i++) {
+                slopes.add(f(g(Xs.get(i))) - f(g(Xs.get(i) + 0.0001)) / -0.0001);
+            }
+            return slopes;
+        }
+        public ArrayList<Double> changeInSlopes(ArrayList <Double> slopes){
+            ArrayList<Double> slopeChanges = new ArrayList<Double>();
+            for (int i = 0; i < slopes.size()-1; i++){
+                slopeChanges.add(Math.abs(slopes.get(i)-slopes.get(i+1)));
+            }
+            return slopeChanges;
+        }
+        public void checkInflectionPoint (ArrayList <Double> slopes, ArrayList <Double> x, ArrayList <Double> y){
+            boolean isNegative = false;
+            for (int i = 0; i < slopes.size(); i++) {
+//            System.out.println(slopes.get(i));
+//                System.out.println(x.get(i) + ", " + y.get(i) + "        " +slopes.get(i));
+                if(x.get(i)-1>-599) {
+                    if (slopes.get(i) - prevSlope < 0 && !isNegative) {
+                        System.out.println("Inflection: " + x.get(i - 1) + ", " + y.get(i - 1));
+                    }
+                    if (slopes.get(i) - prevSlope >= 0 && isNegative) {
+                        System.out.println("Inflection: " + x.get(i - 1) + ", " + y.get(i - 1));
+                    }
                 }
+                if (slopes.get(i) - prevSlope < 0) {
+                    isNegative = true;
+                } else {
+                    isNegative = false;
+                }
+                prevSlope = slopes.get(i);
             }
-            if(!isInvertable){
-                break;
-            }
         }
-        if(isInvertable){
-            g.drawString("IT IS INVERTABLE",500,400);
+        public static void main (String[]args){
+            // Create a window (JFrame) to display the sine wave
+            JFrame frame = new JFrame();
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setSize(1200, 1200);
+            frame.add(new Main());
+            frame.setVisible(true);
         }
-        else{
-            g.drawString("IT IS NOT INVERTABLE",500,400);
+        public static double g ( double x){
+            return x;
+//            return (x*x)/1000;
+//            return 100*Math.sin(x/100);
         }
-    }
+        public static double f ( double x){
+            return x;
+        }
 
-    public static void main(String[] args) {
-        // Create a window (JFrame) to display the sine wave
-        JFrame frame = new JFrame();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(1200, 1200);
-        frame.add(new Main());
-        frame.setVisible(true);
-    }
-    public static double g(double x){
-        return x*x;
-    }
-    public static double f(double x){
-        return x/1000;
-    }
 }
 
